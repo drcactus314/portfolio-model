aa908ebc4534b01d9023a8414f5fa3d4375d8a6d
Artem Kril <arturkril9@gmail.com>
2026-07-22 16:21:25 +0300

    feat: add accordion-js library to the section About me

--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
-import './style/main.scss'
+import "accordion-js/dist/accordion.min.css";
+import Accordion from 'accordion-js';
+
+
+const accordion = document.querySelector('.accordion-container');
+
+const myAccordion = new Accordion(accordion, {
+    openOnInit: [0],
+});
 
 import './js/components/menu.js'
+import './style/main.scss'
+
