5f5f9230fdecab9bdef44d932cf213d527593af9
Artem Kril <arturkril9@gmail.com>
2026-08-03 13:08:30 +0300

    Refactoring: made in contact.js cleaner functions

--- src/js/components/contact.js
+++ src/js/components/contact.js
@@ -1,28 +1,32 @@
 import { allDocument } from "./menu";
 
-const contactForm = document.getElementById('contact-form');
-const backdrop = document.querySelector('.backdrop');
+const contactForm = document.getElementById("contact-form");
+const backdrop = document.querySelector(".backdrop");
 
-
-contactForm.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    backdrop.classList.remove('is-hidden');
-    document.body.style.overflow = 'hidden';
-})
-
-
-const closeModal = ()=>{
-    backdrop.classList.add('is-hidden');
-    document.body.style.overflow = '';
-}
-
-backdrop.addEventListener('click', (e)=>{
-    if(e.target.closest('.modal__close-button') || e.target === backdrop){
-        closeModal();
-    }
-})
-allDocument.addEventListener('keydown', (e)=>{
- if (e.key === 'Escape'){
+const handleEscape = (e) => {
+  if (e.key === "Escape") {
     closeModal();
- }
-})
+  }
+};
+
+const openModal = () => {
+  backdrop.classList.remove("is-hidden");
+  document.body.style.overflow = "hidden";
+  document.addEventListener("keydown", handleEscape());
+};
+const closeModal = () => {
+  backdrop.classList.add("is-hidden");
+  document.body.style.overflow = "";
+  document.removeEventListener("keydown", handleEscape());
+};
+
+contactForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    openModal();
+});
+
+backdrop.addEventListener("click", (e) => {
+  if (e.target.closest(".modal__close-button") || e.target === backdrop) {
+    closeModal();
+  }
+});
